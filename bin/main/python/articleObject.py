d29e680303af4c5cbf69a803ff756a227c329558
Harel <harekl429@gmail.com>
2025-08-06 15:16:43 +0300

    Removed folders and files that should not be in the repository

--- bin/main/python/articleObject.py
+++
@@ -1,8 +0,0 @@
-class Article:
-
-    def __init__(self, title="", body=""):
-        self.title = title
-        self.body = body
-    
-    def __str__(self):
-        return f"Article : [title:\n{self.title}\n,\nbody:\n{self.body}]"
