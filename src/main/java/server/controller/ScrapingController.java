383de70a1d3c338b6f508161536df72dc42cce64
Harel <harekl429@gmail.com>
2025-10-09 19:07:30 +0300

    Web scraping controller- not implemented

---
+++ src/main/java/server/controller/ScrapingController.java
@@ -0,0 +1,22 @@
+package server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import server.helper.ArticleSource;
+
+
+@RestController
+@RequestMapping(path = {"/scrape"})
+public class ScrapingController {
+    
+
+
+    @GetMapping(path="RSS")
+    public ArticleSource[] scrapeRssData()
+    {
+        throw new server.exceptions.UnsupportedOperationException("This function scrapeRssData is not yet implemented");
+    }
+
+}
