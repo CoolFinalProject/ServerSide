c7700e832663ff6b101b33b372b581d6fa63eb48
Harel <harekl429@gmail.com>
2025-10-11 14:48:12 +0300

    Fixed build.gradle and added +x premission for gradlew

--- src/main/java/server/controller/ArticleController.java
+++ src/main/java/server/controller/ArticleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +29,11 @@ public class ArticleController {
 	public ResponseEntity<List<ArticleDto>> getArticlesByText(@RequestParam(name = "text",required = false,defaultValue = "") String text)
 	{
 		return articleService.searchArticlesByText(text);
+	}
+
+	@GetMapping(path= "getArticlesForUser/{id}")
+	public List<ArticleDto> getArticlesForUser(@PathVariable("id") String id)
+	{
+		return null;
 	}
 }
