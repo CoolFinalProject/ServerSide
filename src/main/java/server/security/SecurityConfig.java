c1caca2f1375156e6626d8d53875674259d466b6
Harel <harekl429@gmail.com>
2026-05-29 15:20:45 +0300

    added corsFilter

--- src/main/java/server/security/SecurityConfig.java
+++ src/main/java/server/security/SecurityConfig.java
@@ -1,12 +1,32 @@
 package server.security;
 
+import java.util.Arrays;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        
+        config.setAllowedOrigins(Arrays.asList("http://localhost:8082")); 
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0); 
+        return bean;
+    }
+    
     @Bean
     public FilterRegistrationBean<FirebaseAuthFilter> firebaseAuthFilter() {
         FilterRegistrationBean<FirebaseAuthFilter> reg = new FilterRegistrationBean<>();
